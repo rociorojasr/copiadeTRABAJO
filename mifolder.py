995bca1b81a955a8d727fcd7b0e9e1cf489c6b6e
JennyferRMcda <106215697+JennyferRMcda@users.noreply.github.com>
2022-06-06 12:37:35 -0500

    Update mifolder.py

--- mifolder.py
+++ mifolder.py
@@ -4,7 +4,7 @@ import numpy as np
 
 st.title("EQUIPO 04 - VISUALIZACION DE DATA")
 
-st.text("HOLA")
+st.text("En la presente página se visualizará distintos gráficos con datos relacionados a la evolución de los casos postivos de COVID-19.")
 
 option = st.selectbox(
      'How would you like to be contacted?',
