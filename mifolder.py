cfbff6360b8f8637088fc7a0f12e865530164f34
JennyferRMcda <106215697+JennyferRMcda@users.noreply.github.com>
2022-06-06 12:39:02 -0500

    Update mifolder.py

--- mifolder.py
+++ mifolder.py
@@ -4,7 +4,7 @@ import numpy as np
 
 st.title("EQUIPO 04 - VISUALIZACION DE DATA")
 
-st.text("En la presente página se visualizará distintos gráficos con datos relacionados a la evolución de los casos postivos de COVID-19.")
+st.markdown("En la presente página se visualizará distintos gráficos con datos relacionados a la evolución de los casos postivos de COVID-19.")
 st.markdown('Streamlit is **_really_ cool**.')
 option = st.selectbox(
      'How would you like to be contacted?',
