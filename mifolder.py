bb3681ad5403c70f1a5e817478037c12596b7636
JennyferRMcda <106215697+JennyferRMcda@users.noreply.github.com>
2022-06-06 12:33:55 -0500

    Update mifolder.py

--- mifolder.py
+++ mifolder.py
@@ -4,14 +4,10 @@ import numpy as np
 
 st.title("EQUIPO 04 - VISUALIZACION DE DATA")
 
+st.text("HOLA")
+
 option = st.selectbox(
      'How would you like to be contacted?',
      ('Email', 'Hola', 'Mobile phone'))
 
 st.write('You selected:', option)
-
-uploaded_file = st.file_uploader("Choose a file")
-if uploaded_file is not None:
-     # Can be used wherever a "file-like" object is accepted:
-     dataframe = pd.read_csv(uploaded_file)
-     st.write(dataframe)
