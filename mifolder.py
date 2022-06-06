1a790db6d7cc15bc0808581951d7331d41d5aaa0
JennyferRMcda <106215697+JennyferRMcda@users.noreply.github.com>
2022-06-06 18:19:09 -0500

    Update mifolder.py

--- mifolder.py
+++ mifolder.py
@@ -2,10 +2,11 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title("EQUIPO 04 - VISUALIZACION DE DATA")
+st.title("EQUIPO 04 - VISUALIZACION DE DATA", anchor = None)
 
 st.markdown("En la presente página se visualizará distintos gráficos con datos relacionados a la evolución de los casos postivos de COVID-19.")
 st.markdown("Integrantes:")
+st.markdown("*Sandy Castillo Mallqui, Estefania Huaman Tovar, Maria Rivera Chiclla, Jackeline Roque Maceda y Heydi Surco Mamani*")
 option = st.selectbox(
      'How would you like to be contacted?',
      ('Email', 'Hola', 'Mobile phone'))
