80d98e94f5164e8f7e4df421b75208ec0ac4aee7
JennyferRMcda <106215697+JennyferRMcda@users.noreply.github.com>
2022-06-06 18:31:46 -0500

    Update mifolder.py

--- mifolder.py
+++ mifolder.py
@@ -8,11 +8,15 @@ st.markdown("En la presente página se visualizará distintos gráficos con dato
 st.subheader('Equipo 04')
 st.markdown("Integrantes:")
 st.markdown("*Sandy Castillo Mallqui, Estefania Huaman Tovar, Maria Rivera Chiclla, Jackeline Roque Maceda y Heydi Surco Mamani*")
-option = st.selectbox(
-     'How would you like to be contacted?',
-     ('Email', 'Hola', 'Mobile phone'))
 
-st.write('You selected:', option)
+#####CONTEXTO
+st.subheader("Contexto:")
+st.markdown("textooooo")
 from PIL import Image
 image = Image.open('covid.jpg')
 st.image(image)
+
+option = st.selectbox(
+     'How would you like to be contacted?',
+     ('Email', 'Hola', 'Mobile phone'))
+st.write('You selected:', option)
