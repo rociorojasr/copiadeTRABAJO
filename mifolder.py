9dd4c415e7d746ba99c9245af181c254f59b0334
JennyferRMcda <106215697+JennyferRMcda@users.noreply.github.com>
2022-06-06 11:34:05 -0500

    Add files via upload

---
+++ mifolder.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.title("EQUIPO 07 - VISUALIZACION DE DATA")
+
+option = st.selectbox(
+     'How would you like to be contacted?',
+     ('Email', 'Home phone', 'Mobile phone'))
+
+st.write('You selected:', option)
+
+uploaded_file = st.file_uploader("Choose a file")
+if uploaded_file is not None:
+     # Can be used wherever a "file-like" object is accepted:
+     dataframe = pd.read_csv(uploaded_file)
+     st.write(dataframe)
