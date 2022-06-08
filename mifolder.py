8b7d33c8bbccd469d3d4317050733159df313844
JennyferRMcda <106215697+JennyferRMcda@users.noreply.github.com>
2022-06-07 19:15:26 -0500

    Update mifolder.py

--- mifolder.py
+++ mifolder.py
@@ -6,7 +6,6 @@ st.title("      Casos positivos COVID-19", anchor = None)
 
 st.markdown("En la presente página se visualizará distintos gráficos con datos relacionados a la evolución de los casos postivos de COVID-19.")
 st.subheader('Equipo 04')
-st.markdown("Integrantes:")
 st.markdown("*Sandy Castillo Mallqui, Estefania Huaman Tovar, Maria Rivera Chiclla, Jackeline Roque Maceda y Heydi Surco Mamani*")
 
 #####CONTEXTO
