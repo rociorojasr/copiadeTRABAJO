f9eda29be47b9c2f8710c1fc3f964ad20631e86e
JennyferRMcda <106215697+JennyferRMcda@users.noreply.github.com>
2022-06-09 11:25:53 -0500

    Update mifolder.py

--- mifolder.py
+++ mifolder.py
@@ -1,6 +1,14 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import urllib.request
+@st.experimental_memo
+def download_data():
+ url = 'https://files.minsa.gob.pe/s/eRqxR35ZCxrzNgr/download'
+ filename = 'data.csv'
+ urllib.request.urlretrieve(url, filename)
+download_data()
+
 
 st.title("      Casos positivos COVID-19", anchor = None)
 
