da5f124f8ffc5bd0d5d8d44dc404072515119f0f
JennyferRMcda <106215697+JennyferRMcda@users.noreply.github.com>
2022-06-11 20:33:07 -0500

    Update mifolder.py

--- mifolder.py
+++ mifolder.py
@@ -5,9 +5,11 @@ import urllib.request
 @st.experimental_memo
 def download_data():
  url = 'https://files.minsa.gob.pe/s/eRqxR35ZCxrzNgr/download'
+ df = pd.read_csv(url,index_col=0,parse_dates=[0])
  filename = 'positivos_covid.csv'
  urllib.request.urlretrieve(url, filename)
 download_data()
+print df
 st.title("      Casos positivos COVID-19", anchor = None)
 
 st.markdown("En la presente página se visualizará distintos gráficos con datos relacionados a la evolución de los casos postivos de COVID-19.")
@@ -21,8 +23,7 @@ from PIL import Image    ######Insertar imagen en streamlit
 image = Image.open('covid.jpg')
 st.image(image)
 
-df = pd.read_csv(filename)
-df
+
 
 ##st.line_chart(df[['FECHA_RESULTADO', 'METODODX']].groupby('FECHA_RESULTADO').count().rolling(window = 1, center =False))
 
