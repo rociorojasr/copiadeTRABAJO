c2e7b7172163f1775a08176891ff414fe761ed55
JennyferRMcda <106215697+JennyferRMcda@users.noreply.github.com>
2022-06-13 12:31:36 -0500

    Create mifolder.py

--- mifolder.py
+++ mifolder.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import gdown
 
+
 #id = 1Gu65mnJ_lxE0BdbkL1nTq5qaFJ1dJ9tq
 @st.experimental_memo
 def download_data():
@@ -13,20 +14,18 @@ def download_data():
 
 download_data()
 data = pd.read_csv('data.csv', sep = ';', nrows = 1000000, parse_dates=['FECHA_CORTE', 'FECHA_RESULTADO'])
-st.dataframe(data.head(20))
+
 
 st.title("      Casos positivos COVID-19", anchor = None )
+
 st.markdown("En la presente página se visualizará distintos gráficos con datos relacionados a la evolución de los casos postivos de COVID-19.")
 st.subheader('Equipo 04')
 st.markdown("*Sandy Castillo Mallqui, Estefania Huaman Tovar, Maria Rivera Chiclla, Jackeline Roque Maceda y Heydi Surco Mamani*")
-
-
-#####CONTEXTO
 st.subheader("Contexto:")
 st.markdown("textooooo")
 from PIL import Image    ######Insertar imagen en streamlit
 image = Image.open('covid.jpg')
 st.image(image)
-metodo=data['METODODX']
-st.line_chart(metodo)
-#df[['FECHA_RESULTADO', 'METODODX']].groupby('FECHA_RESULTADO').count().rolling(window = 1, center =False).mean().plot()
+
+st.dataframe(data.head(20))
+
