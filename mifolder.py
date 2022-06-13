fa14b0e45741f65d5232d4c0412e9d5b983f2152
estefaniaht <106989387+estefaniaht@users.noreply.github.com>
2022-06-13 12:08:40 -0500

    Update mifolder.py

--- mifolder.py
+++ mifolder.py
@@ -12,7 +12,7 @@ def download_data():
  gdown.download(url,output,quiet = False)
 
 download_data()
-data = pd.read_csv('data.csv', sep = ';', nrows = 1000000, parse_dates=['FECHA_CORTE'])
+data = pd.read_csv('data.csv', sep = ';', nrows = 1000000, parse_dates=['FECHA_CORTE', 'FECHA_RESULTADO'])
 st.dataframe(data.head(20))
 
 
