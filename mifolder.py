c10ae3393a4dfc0554f8d760d1c758573a47edf8
estefaniaht <106989387+estefaniaht@users.noreply.github.com>
2022-06-13 12:34:01 -0500

    Create mifolder.py

--- mifolder.py
+++ mifolder.py
@@ -3,7 +3,6 @@ import pandas as pd
 import numpy as np
 import gdown
 
-
 #id = 1Gu65mnJ_lxE0BdbkL1nTq5qaFJ1dJ9tq
 @st.experimental_memo
 def download_data():
@@ -17,15 +16,19 @@ data = pd.read_csv('data.csv', sep = ';', nrows = 1000000, parse_dates=['FECHA_C
 
 
 st.title("      Casos positivos COVID-19", anchor = None )
-
 st.markdown("En la presente página se visualizará distintos gráficos con datos relacionados a la evolución de los casos postivos de COVID-19.")
 st.subheader('Equipo 04')
 st.markdown("*Sandy Castillo Mallqui, Estefania Huaman Tovar, Maria Rivera Chiclla, Jackeline Roque Maceda y Heydi Surco Mamani*")
+
+
+#####CONTEXTO
 st.subheader("Contexto:")
 st.markdown("textooooo")
 from PIL import Image    ######Insertar imagen en streamlit
 image = Image.open('covid.jpg')
+
 st.image(image)
-
 st.dataframe(data.head(20))
-
+edad=data['EDAD']
+st.line_chart(edad)
+#df[['FECHA_RESULTADO', 'METODODX']].groupby('FECHA_RESULTADO').count().rolling(window = 1, center =False).mean().plot()
