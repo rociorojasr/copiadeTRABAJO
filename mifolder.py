7e9acd2122c22266032604d967185568b8d7a662
JennyferRMcda <106215697+JennyferRMcda@users.noreply.github.com>
2022-06-13 12:21:02 -0500

    Update mifolder.py

--- mifolder.py
+++ mifolder.py
@@ -20,12 +20,10 @@ st.title("      Casos positivos COVID-19", anchor = None )
 st.markdown("En la presente página se visualizará distintos gráficos con datos relacionados a la evolución de los casos postivos de COVID-19.")
 st.subheader('Equipo 04')
 st.markdown("*Sandy Castillo Mallqui, Estefania Huaman Tovar, Maria Rivera Chiclla, Jackeline Roque Maceda y Heydi Surco Mamani*")
-
-st.dataframe(data.head(20))
-
-#####CONTEXTO
 st.subheader("Contexto:")
 st.markdown("textooooo")
 from PIL import Image    ######Insertar imagen en streamlit
 image = Image.open('covid.jpg')
 st.image(image)
+
+st.dataframe(data.head(20))
