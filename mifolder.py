047a62bc7c8a92d560e0393575b913c62621fdee
JennyferRMcda <106215697+JennyferRMcda@users.noreply.github.com>
2022-06-13 12:16:48 -0500

    Update mifolder.py

--- mifolder.py
+++ mifolder.py
@@ -13,14 +13,13 @@ def download_data():
 
 download_data()
 data = pd.read_csv('data.csv', sep = ';', nrows = 1000000, parse_dates=['FECHA_CORTE', 'FECHA_RESULTADO'])
+st.dataframe(data.head(20))
 
 st.title("      Casos positivos COVID-19", anchor = None )
 st.markdown("En la presente página se visualizará distintos gráficos con datos relacionados a la evolución de los casos postivos de COVID-19.")
 st.subheader('Equipo 04')
 st.markdown("*Sandy Castillo Mallqui, Estefania Huaman Tovar, Maria Rivera Chiclla, Jackeline Roque Maceda y Heydi Surco Mamani*")
 
-
-st.dataframe(data.head(20))
 
 #####CONTEXTO
 st.subheader("Contexto:")
