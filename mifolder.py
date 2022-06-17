d3beea236cd1c0b7561bbfaeef84a491a26e9a36
sandycas <106991369+sandycas@users.noreply.github.com>
2022-06-17 00:23:07 -0500

    Update mifolder.py

--- mifolder.py
+++ mifolder.py
@@ -41,7 +41,7 @@ explode = (0, 0.1, 0, 0)
 fig1, ax1 = plt.subplots()
 ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
         shadow=True, startangle=90)
-ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+ax1.axis('equal')  
 
 st.pyplot(fig1)
 
