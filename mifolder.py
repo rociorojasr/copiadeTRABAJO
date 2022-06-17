fec3acba517924c405f9d1560317e86b4e2877a7
sandycas <106991369+sandycas@users.noreply.github.com>
2022-06-17 00:13:04 -0500

    Update mifolder.py

--- mifolder.py
+++ mifolder.py
@@ -31,6 +31,15 @@ metodo=data['METODODX']
 ######## GRÁFICO 01
 A= data[['METODODX', 'SEXO']].groupby('METODODX').count().rename(columns = {'SEXO': 'count'})
 A
+labels = 'AG', 'PCR', 'PR',
+sizes = [446144,29722,256634]
+explode = (0, 0.1, 0, 0)  
+fig1, ax1 = plt.subplots()
+ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
+        shadow=True, startangle=90)
+ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+
+st.pyplot(fig1)
 
 
 
