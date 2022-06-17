a4bda0e216e5a11c8e658d29004f696a3e816a6f
sandycas <106991369+sandycas@users.noreply.github.com>
2022-06-17 00:39:56 -0500

    Update mifolder.py

--- mifolder.py
+++ mifolder.py
@@ -33,7 +33,7 @@ metodo=data['METODODX']
 ######## GRÁFICO 01
 A= data[['METODODX', 'SEXO']].groupby('METODODX').count().rename(columns = {'SEXO': 'count'})
 A
-
+import matplotlib.pyplot as plt
 labels = 'AG', 'PCR', 'PR',
 sizes = [446144,29722,256634]
 explode = (0, 0.1, 0, 0)  
