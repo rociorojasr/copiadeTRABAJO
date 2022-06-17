a0d269a8e658252f3206210d7ddcd2485195d896
sandycas <106991369+sandycas@users.noreply.github.com>
2022-06-17 11:47:30 -0500

    Update mifolder.py

--- mifolder.py
+++ mifolder.py
@@ -35,15 +35,6 @@ A= data[['METODODX', 'SEXO']].groupby('METODODX').count().rename(columns = {'SEX
 A
 
 
-From matplotlib import pyplot as plt
-labels = 'AG', 'PCR', 'PR',
-sizes = [446144,29722,256634]
-explode = (0, 0.1, 0, 0)  
-fig1, ax1 = plt.subplots()
-ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
-        shadow=True, startangle=90)
-ax1.axis('equal')  
-st.pyplot(fig1)
 
 
 
