ae764b6bc9193ab9b9b0f739504582a3a7b8fd39
rociorojasr <117869256+rociorojasr@users.noreply.github.com>
2022-12-09 12:26:15 -0500

    Update trabajo.py

--- trabajo.py
+++ trabajo.py
@@ -1,16 +1,17 @@
 import gdown
 import streamlit as st
 import pandas as pd
-import numpy as np
+
 #id=1WUcvXmNN6nuDbtv0crOe2qDrbc9stWjA
 @st.experimental_memo
 def download_data():
-	#https://drive.google.com/uc?id=MIOOO
-	url="https://drive.google.com/uc?id=1WUcvXmNN6nuDbtv0crOe2qDrbc9stWjA"
-	output="data.csv"
-	gdown.download(url,output,quiet= False)
+  #https://drive.google.com/uc?id=MIOOO
+  url="https://drive.google.com/uc?id=1WUcvXmNN6nuDbtv0crOe2qDrbc9stWjA"
+  output='data.csv'
+  gdown.download(url,output,quiet= False)
+	
 download_data()
-df = pd.read_csv('data.csv',sep = ";",  skip_blank_lines=True, nrows=25,parse_dates=['Departamento', '#Centro de vacunación'])
+df = pd.read_csv('data.csv',sep = ';',nrows=25,parse_dates=['Departamento', '#Centro de vacunación'])
 st.dataframe(data.head(5))
 
 
