1f4f0a4e64ce1a9386e75253b9466fbe4462cfb9
rociorojasr <117869256+rociorojasr@users.noreply.github.com>
2022-12-09 11:33:21 -0500

    Update mifolder.py

--- mifolder.py
+++ mifolder.py
@@ -6,31 +6,17 @@ from pyecharts import options as opts
 from pyecharts.charts import Bar
 from streamlit_echarts import st_pyecharts
 from streamlit_echarts import st_echarts
-####################################################################################
-##id = 1Gu65mnJ_lxE0BdbkL1nTq5qaFJ1dJ9tq
-#@st.experimental_memo
-#def download_data():
-#     url = "https://drive.google.com/uc?id=1Gu65mnJ_lxE0BdbkL1nTq5qaFJ1dJ9tq"
-###########################################
 
-#id=13iNig4VIvt5Gm0znUt2eq3_YnGCgCQHM
-@st.experimental_nemo
+#id = 1Gu65mnJ_lxE0BdbkL1nTq5qaFJ1dJ9tq
+@st.experimental_memo
 def download_data():
-	#https://drive.google.com/uc?id=MIOOO
-	url="https://drive.google.com/uc?id=13iNig4VIvt5Gm0znUt2eq3_YnGCgCQHM"
-	output="data.csv"
-	gdown.download(url,output,quiet=False)
+     url = "https://drive.google.com/uc?id=1Gu65mnJ_lxE0BdbkL1nTq5qaFJ1dJ9tq"
+     output = 'data.csv'
+     gdown.download(url,output,quiet=False)
 download_data()
-#df = pd.read_csv(r'C:\Users\51952\Downloads\PositivosCovid\positivos_covid.csv',sep=";", skip_blank_lines=True, parse_dates=['id_centro_vacunacion', 'id_eess'])
-df = pd.read_csv('data.csv',sep = ";",  skip_blank_lines=True, nrows=1000,parse_dates=['id_centro_vacunacion', 'id_eess'])
-st.dataframe(data.head(20))
-#########################################
-#     output = 'data.csv'
-#     gdown.download(url,output,quiet=False)
-#download_data()
-##df = pd.read_csv(r'C:\Users\51952\Downloads\PositivosCovid\positivos_covid.csv',sep=";", skip_blank_lines=True, parse_dates=['FECHA_CORTE', 'FECHA_RESULTADO'])
+#df = pd.read_csv(r'C:\Users\51952\Downloads\PositivosCovid\positivos_covid.csv',sep=";", skip_blank_lines=True, parse_dates=['FECHA_CORTE', 'FECHA_RESULTADO'])
 
-#df = pd.read_csv('data.csv',sep = ";",  skip_blank_lines=True, nrows=1000000,parse_dates=['FECHA_CORTE', 'FECHA_RESULTADO'])
+df = pd.read_csv('data.csv',sep = ";",  skip_blank_lines=True, nrows=1000000,parse_dates=['FECHA_CORTE', 'FECHA_RESULTADO'])
 
 st.title("      CASOS POSITIVOS: COVID-19", anchor = None )
 from PIL import Image
@@ -119,4 +105,3 @@ elif option == "Gráfico de dispersión por FECHA DE RESULTADOS":
     st.subheader("Gráfico de dispersión por FECHA DE RESULTADOS")
     L = df[['FECHA_RESULTADO', 'METODODX']].groupby('FECHA_RESULTADO').count()
     st.line_chart(L)
-
