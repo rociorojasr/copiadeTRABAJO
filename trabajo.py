da807f10ee8c3924626a474da7af219840eb7c0b
rociorojasr <117869256+rociorojasr@users.noreply.github.com>
2022-12-09 12:02:19 -0500

    Update trabajo.py

--- trabajo.py
+++ trabajo.py
@@ -14,17 +14,21 @@ from streamlit_echarts import st_echarts
 ###########################################
 
 #id=13iNig4VIvt5Gm0znUt2eq3_YnGCgCQHM
+#id=15qMTN9YVwTvhyI0PvoobhL303iU2LhtU
 @st.experimental_memo
 def download_data():
 	#https://drive.google.com/uc?id=MIOOO
-	url="https://drive.google.com/uc?id=13iNig4VIvt5Gm0znUt2eq3_YnGCgCQHM"
+	url="https://drive.google.com/uc?id=15qMTN9YVwTvhyI0PvoobhL303iU2LhtU"
 	output="data.csv"
 	gdown.download(url,output,quiet=False)
 download_data()
 #df = pd.read_csv(r'C:\Users\51952\Downloads\PositivosCovid\positivos_covid.csv',sep=";", skip_blank_lines=True, parse_dates=['id_centro_vacunacion', 'id_eess'])
+#df = pd.read_csv('data.csv',sep = ";",  skip_blank_lines=True, nrows=1000000,parse_dates=['FECHA_CORTE', 'FECHA_RESULTADO'])
 df = pd.read_csv('data.csv',sep = ";",  skip_blank_lines=True, nrows=1000,parse_dates=['id_centro_vacunacion', 'id_eess'])
 st.dataframe(data.head(20))
 #########################################
-
+#C:\Users\ROCIO\Downloads\positivos_covid.csv
+#C:\Users\ROCIO\Downloads\datos final.xlsx
+#C:\Users\ROCIO\Downloads\datos_final.csv
 
 
